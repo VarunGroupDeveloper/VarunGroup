b70feaa21e803ebdcc3ccc5d0c53d59199a726b1
vsravani-dev <sravani87878@gmail.com>
2025-08-21 10:11:05 +0530

    page added

--- application/views/templates/home/pages/hospitality.php
+++ application/views/templates/home/pages/hospitality.php
@@ -323,19 +323,19 @@
                                                     <div class="rs-services-tab-btn">
                                                         <a class="rs-btn has-text has-icon" href="services-details.html">Read More
                                                             <span class="icon-box">
-                                                <svg class="icon-first" xmlns="http://www.w3.org/2000/svg"
-                                                   viewBox="0 0 32 32">
-                                                   <path
-                                                      d="M31.71,15.29l-10-10L20.29,6.71,28.59,15H0v2H28.59l-8.29,8.29,1.41,1.41,10-10A1,1,0,0,0,31.71,15.29Z">
-                                                   </path>
-                                                </svg>
-                                                <svg class="icon-second" xmlns="http://www.w3.org/2000/svg"
-                                                   viewBox="0 0 32 32">
-                                                   <path
-                                                      d="M31.71,15.29l-10-10L20.29,6.71,28.59,15H0v2H28.59l-8.29,8.29,1.41,1.41,10-10A1,1,0,0,0,31.71,15.29Z">
-                                                   </path>
-                                                </svg>
-                                             </span>
+                                                                <svg class="icon-first" xmlns="http://www.w3.org/2000/svg"
+                                                                viewBox="0 0 32 32">
+                                                                <path
+                                                                    d="M31.71,15.29l-10-10L20.29,6.71,28.59,15H0v2H28.59l-8.29,8.29,1.41,1.41,10-10A1,1,0,0,0,31.71,15.29Z">
+                                                                </path>
+                                                                </svg>
+                                                                <svg class="icon-second" xmlns="http://www.w3.org/2000/svg"
+                                                                viewBox="0 0 32 32">
+                                                                <path
+                                                                    d="M31.71,15.29l-10-10L20.29,6.71,28.59,15H0v2H28.59l-8.29,8.29,1.41,1.41,10-10A1,1,0,0,0,31.71,15.29Z">
+                                                                </path>
+                                                                </svg>
+                                                            </span>
                                                         </a>
                                                     </div>
                                                 </div>
