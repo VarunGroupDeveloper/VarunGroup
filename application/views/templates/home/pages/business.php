a9c3a14b1cb3e0b10fdb19289d3fdc8501f80d4a
kbhargavi-dev <kelavathubhargavi@gmail.com>
2025-10-27 11:38:11 +0530

    bussiness page changes

--- application/views/templates/home/pages/business.php
+++ application/views/templates/home/pages/business.php
@@ -108,8 +108,7 @@
                                         <span class="prefix">+</span>
                                     </div>
                                 </div>
-                                <span class="rs-counter-title"><span class="large-text">Number of</span> Employees<br>
-                           </span>
+                                <span class="rs-counter-title"><span class="small-text">Number of Employees</span>
                             </div>
                         </div>
                     </div>
@@ -131,8 +130,7 @@
                                         <span class="prefix">+</span>
                                     </div>
                                 </div>
-                                <span class="rs-counter-title"><span class="large-text">Customer</span> Touchpoints <br>
-                           </span>
+                                <span class="rs-counter-title"><span class="small-text">Customer Touchpoints</span> 
                             </div>
                         </div>
                     </div>
@@ -150,12 +148,11 @@
                                         </svg>
                                     </div>
                                     <div class="rs-counter-number-item">
-                                        <span class="rs-counter-number odometer" data-count="250000">00</span>
+                                        <span class="rs-counter-number odometer" data-count="25lakh">00</span>
                                         <span class="prefix">+</span>
                                     </div>
                                 </div>
-                                <span class="rs-counter-title"><span class="large-text">Vehicles</span> Serviced Annually<br>
-                           </span>
+                                <span class="rs-counter-title"><span class="small-text">Vehicles Serviced Anually</span> 
                             </div>
                         </div>
                     </div>
@@ -177,7 +174,7 @@
                                         <span class="prefix">+</span>
                                     </div>
                                 </div>
-                                <span class="rs-counter-title"><span class="large-text">Vehicles </span> sold Annually<br>
+                                <span class="rs-counter-title"><span class="small-text">Vehicles sold Annually<br>
                            </span>
                             </div>
                         </div>
